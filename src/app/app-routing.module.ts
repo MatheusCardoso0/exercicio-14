897383c8bef626d2d9029b408e5bf76393e2a6e9
MatheusCardoso0 <mathde12@gmail.com>
2021-10-25 14:15:55 -0300

    Ajustes finais

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { FilmeListComponent2 } from './filme-2/filme-list-2/filme-2-list.component';
 import { FilmeListComponent } from './filme/filme-list/filme-list.component';
 import { HomeComponent } from './home/home.component';
 
@@ -9,7 +8,6 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
   { path: 'lista-filmes', component: FilmeListComponent },
-  { path: 'lista-2-filmes', component: FilmeListComponent2 }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
